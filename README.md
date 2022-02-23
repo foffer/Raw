89cefe55e4d6a57eef0ad9588fe9a02467c4988e
Christoffer Buusmann <foffer@gmail.com>
2022-02-23 12:18:08 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Raw
+
+A description of this package.
---
+++ Tests/RawTests/RawTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Raw
+
+final class RawTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Raw().text, "Hello, World!")
+    }
+}
